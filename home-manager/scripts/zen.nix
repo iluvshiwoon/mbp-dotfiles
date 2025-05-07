3fa67554b487e0051c64af14c9142968a0076714
iluvshiwoon <sntcillian@gmail.com>
2025-05-08 00:39:32 +0800

    zen-profile entry

--- home-manager/scripts/zen.nix
+++ home-manager/scripts/zen.nix
@@ -1,34 +1,17 @@
 { config, pkgs, inputs,  ... }:
 
 let
-  zenProfileName = "l11nmepi.Default (release)"; # Replace with your profile name
-  zenProfileDir = "${inputs.zen-profile}/${zenProfileName}";
-  
-  # Location where zen stores profiles on macOS
-  zenProfilesDir = "$HOME/Library/Application Support/zen/Profiles";
-  
-  # Destination path for the profile
-  destProfilePath = "${zenProfilesDir}/${zenProfileName}";
+  zen_dir = "$HOME/Library/Application Support/zen";
   
   # Script to deploy the zen profile
   synczenProfile = pkgs.writeShellScriptBin "sync-zen-profile" ''
     #!/usr/bin/env bash
     set -euo pipefail
-    
-    zen_DIR="${zenProfilesDir}"
-    PROFILE_PATH="${destProfilePath}"
-    
-    echo "Checking for zen profile at $PROFILE_PATH"
-    
-    if [ ! -d "$PROFILE_PATH" ]; then
-      echo "Profile does not exist. Deploying from flake input..."
-      
-      # Ensure the zen profiles directory exists
-      mkdir -p "$zen_DIR"
-      
+    mkdir -p "${zen_dir}"
+    if [ -z '$(ls -A "${zen_dir}")' ]; then
+      echo "zen config empty ! copying files..."
       # Copy the profile from the flake input to the zen directory
-      cp -r "${zenProfileDir}" "$PROFILE_PATH"
-      
+      cp -r "${inputs.zen-profile}" "${zen_dir}"
       echo "zen profile has been deployed successfully!"
     else
       echo "zen profile already exists. No action taken."
